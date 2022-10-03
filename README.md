668278f2876afc04eb3baf099ca044186e80e83f
Davina Leong <davina.leong.sy@gmail.com>
2022-10-03 14:37:03 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# proj-proverbs-everyday-2-app
+ Proverbs Everyday App V2
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./styles/main.scss"
+
+function App() {
+  return (
+    <main className="main-grid">
+      <header>[Header]</header>
+
+      <div>[Content]</div>
+
+      <footer>[Footer]</footer>
+    </main>
+  )
+}
+
+export default App
